f81fca48c03d6ae557f47aaa0bf43ba00f3ea5f7
wwelisa <elisa.bauer15@gmail.com>
2022-11-01 21:16:25 +0100

    image msg to cv::Mat

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,10 +1,20 @@
 #include "ros/ros.h"    //The ROS main header
 #include <math.h>
 #include <iostream>
+#include "opencv2/opencv.hpp"
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 
 // Messages
 #include "std_msgs/String.h"
 #include "sensor_msgs/Image.h"  //http://docs.ros.org/en/noetic/api/sensor_msgs/html/msg/Image.html
+
+
+
 
 
 class Camera
@@ -17,23 +27,49 @@ class Camera
         float vw_odom_now;      ///< Fahrbefehl (Odometrie): Winkelgeschwindigkeit [rad/s] um Z
 
         std_msgs::String output;  ///< Pose mit Kovarianz, welche gepublisht wird und in RVIZ dargestellt
+        cv::Mat m_cameraImage;
 
         //----------------------Methoden----------------------//
         Camera();
-        void callbackCamera(const sensor_msgs::Image& cameraImage);
+        void callbackCamera(const sensor_msgs::ImageConstPtr& cameraImage);
 
     private:
         ros::Publisher tf_pub;    ///< Publisher für die erkannte Kugelposition
         ros::Subscriber sub_camera;      ///< Subscriber für die Kamera
-
+        cv::Mat imageCb(const sensor_msgs::ImageConstPtr& msg);
 };
 
+cv::Mat Camera::imageCb(const sensor_msgs::ImageConstPtr& msg)
+  {
+    cv_bridge::CvImagePtr cv_ptr;
+    cv::Mat image;
+    try
+    {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+      image = cv_ptr->image;
+      return image;
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return image;
+    }
+  }
 
-void Camera::callbackCamera(const sensor_msgs::Image& cameraImage)
+void Camera::callbackCamera(const sensor_msgs::ImageConstPtr& cameraImage)
 {
-    std::cout << "CALLBACK" << std::endl;
-    std::cout << "Bildbreite: " << cameraImage.width << "\tBildhöhe: " << cameraImage.height << std::endl;
+    //std::cout << "CALLBACK" << std::endl;
+    //std::cout << "Bildbreite: " << cameraImage.width << "\tBildhöhe: " << cameraImage.height << std::endl;
     //output = cameraImage.width.toString();
+    if(cameraImage->width != 0){
+        m_cameraImage = imageCb(cameraImage);
+        std::cout << "CALLBACK" << std::endl;
+        //std::cout << "cam: " << m_cameraImage << std::endl;
+        //cv::namedWindow("test", 0);  
+        cv::imshow("test", m_cameraImage);
+        cv::waitKey(2000); 
+
+    }
     output.data = "text";
     tf_pub.publish(output);
 }
@@ -57,6 +93,8 @@ int main(int argc, char** argv)
     
     Camera cam;
     std::cout << "MAIN" << std::endl;
+
+
 
     ros::spin();
 }
