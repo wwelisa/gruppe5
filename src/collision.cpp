683882e7367610019d9095751e22ec50f4e3348b
PhitschiPro <p.salner2@gmail.com>
2023-01-22 22:35:43 +0100

    changed the timer

--- src/collision.cpp
+++ src/collision.cpp
@@ -10,7 +10,7 @@
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 
-#define MAX_TIMER_SEC 30 // time [sec] 10 minutes
+#define MAX_TIMER_SEC 300 // max time [sec] until shutdown
 #define TOLERANCE 0.35  // distance [m] between robots needed to be considered a successful catch
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr &odom_1_data, const nav_msgs::Odometry::ConstPtr &odom_2_data, double startTime)
